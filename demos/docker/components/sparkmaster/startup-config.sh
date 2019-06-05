856a1458cc768a99d8dfd884acfff8bb5dbc94d3
Ronal Muresano <rmuresano@iti.es>
2019-06-05 15:40:01 +0200

    Supported services

--- demos/docker/components/sparkmaster/startup-config.sh
+++ demos/docker/components/sparkmaster/startup-config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-echo -e "\e[31mSPARK MASTER\e[32m => IP:$IP, MASTER_PORT:$MASTER_PORT, MASTER_WEBUI_PORT:$MASTER_WEBUI_PORT, DRIVER_PORT:$DRIVER_PORT\e[39m"
+echo -e "\e[31mSPARK MASTER\e[32m => IP:$IP, MASTER_PORT:$SPARK_MASTER_PORT, MASTER_WEBUI_PORT:$SPARK_MASTER_WEBUI_PORT\e[39m"
 
 export RUNTIME_DIR=/home/legacy
 export JAVA_HOME="$RUNTIME_DIR/jre"
@@ -27,25 +27,23 @@ if  [[ $HDFS == "YES" ]]; then
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
     <?xml-stylesheet type=\"text/xsl\" href=\"configuration.xsl\"?>
     <configuration>
-      <property><name>dfs.webhdfs.enabled</name><value>true</value></property>
-      <property><name>dfs.permissions.enabled</name><value>false</value></property>
-      <property><name>dfs.client.use.datanode.hostname</name><value>false</value></property>
-      <property><name>dfs.datanode.use.datanode.hostname</name><value>false</value></property>
+        <property><name>dfs.webhdfs.enabled</name><value>true</value></property>
+        <property><name>dfs.permissions.enabled</name><value>false</value></property>
+        <property><name>dfs.client.use.datanode.hostname</name><value>false</value></property>
+        <property><name>dfs.datanode.use.datanode.hostname</name><value>false</value></property>
 
-      <property><name>dfs.namenode.name.dir</name><value>file:///hdfsdata/nameNode/</value></property>
-      <property><name>dfs.namenode.checkpoint.dir</name><value>$HADOOP_HOME/data/secondary</value></property>
-      <property><name>dfs.namenode.http-adress</name><value>$HOSTNAME:$HDFS_NAMENODE_WEBUI_PORT</value></property>
-      <property><name>dfs.namenode.datanode.registration.ip-hostname-check</name><value>false</value></property>
+        <property><name>dfs.namenode.name.dir</name><value>file:///data/nameNode/</value></property>
+        <property><name>dfs.namenode.checkpoint.dir</name><value>$HADOOP_HOME/data/secondary</value></property>
+        <property><name>dfs.namenode.http-adress</name><value>$HOSTNAME:$HDFS_NAMENODE_WEBUI_PORT</value></property>
+        <property><name>dfs.namenode.datanode.registration.ip-hostname-check</name><value>false</value></property>
+        <property> <name>dfs.replication</name> <value>$HDFS_REPLICATION_FACTOR</value></property>
 
-      <property> <name>dfs.replication</name> <value>$HDFS_REPLICATION_FACTOR</value></property>
-
-      <property> <name>dfs.webhdfs.enabled</name><value>true</value></property>
 
     </configuration>" > $HADOOP_HOME/etc/hadoop/hdfs-site.xml
 
     echo "Namenode"
 
-    if [ -e /hdfsdata/nameNode/current/VERSION ]
+    if [ -e /data/nameNode/current/VERSION ]
     then
         echo "Using a Previuslly formatted HDFS file system"
     else
--- demos/docker/components/sparkworker/startup-config.sh
+++ demos/docker/components/sparkworker/startup-config.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo -e "\e[31mSPARK MASTER\e[32m => IP:$IP, MASTER_PORT:$SPARK_MASTER_PORT, MASTER_WEBUI_PORT:$SPARK_MASTER_WEBUI_PORT \e[39m"
 export RUNTIME_DIR=/home/legacy
 export JAVA_HOME="$RUNTIME_DIR/jre"
 export SPARK_HOME="$RUNTIME_DIR/spark"
@@ -7,11 +8,10 @@ export HADOOP_HOME="$RUNTIME_DIR/hadoop"
 export PATH="$PATH:$JAVA_HOME/bin:$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$SPARK_HOME/bin:$HADOOP_CONF_DIR:$PATH"
 
 echo "export SPARK_HOME=$SPARK_HOME" >> $HOME/.bashrc
+echo "export PYTHONPATH=$SPARK_HOME/python/:$SPARK_HOME/python/lib/py4j-0.10.6-src.zip:$PYTHONPATH" >> $HOME/.bashrc
 
 
 export SPARK_DIST_CLASSPATH=$(hadoop classpath)
-
-mkdir $RUNTIME_DIR/data/nameNode
 
 if  [[ $HDFS == "YES" ]]; then
   # Set up the core-site Xml
@@ -29,16 +29,16 @@ if  [[ $HDFS == "YES" ]]; then
   echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
   <?xml-stylesheet type=\"text/xsl\" href=\"configuration.xsl\"?>
   <configuration>
-    <property><name>dfs.permissions.enabled</name><value>false</value></property>
-    <property><name>dfs.client.use.datanode.hostname</name><value>false</value></property>
-    <property><name>dfs.datanode.use.datanode.hostname</name><value>false</value></property>
+      <property><name>dfs.webhdfs.enabled</name><value>true</value></property>
+      <property><name>dfs.permissions.enabled</name><value>false</value></property>
+      <property><name>dfs.client.use.datanode.hostname</name><value>false</value></property>
+      <property><name>dfs.datanode.use.datanode.hostname</name><value>false</value></property>
 
-    <property><name>dfs.namenode.name.dir</name><value>file:///$RUNTIME_DIR/data/nameNode</value></property>
-    <property><name>dfs.namenode.http-adress</name><value>$NAMENODE_HOSTNAME:$HDFS_NAMENODE_WEBUI_PORT</value></property>
-
-    <property><name>dfs.datanode.data.dir</name><value>file:///hdfsdata/dataNode</value></property>
-    <property> <name>dfs.replication</name> <value>$HDFS_REPLICATION_FACTOR</value></property>
-    <property> <name>dfs.webhdfs.enabled</name><value>true</value></property>
+      <property><name>dfs.namenode.name.dir</name><value>file:///data/nameNode</value></property>
+      <property><name>dfs.namenode.http-address</name><value>$HDFS_NAMENODE_HOSTNAME:$HDFS_NAMENODE_WEBUI_PORT</value></property>
+      <property><name>dfs.datanode.data.dir</name><value>file:///data/dataNode</value></property>
+      <property><name>dfs.namenode.http-adress</name><value>$HOSTNAME:$HDFS_NAMENODE_WEBUI_PORT</value></property>
+      <property> <name>dfs.replication</name> <value>$HDFS_REPLICATION_FACTOR</value></property>
   </configuration>" > $HADOOP_HOME/etc/hadoop/hdfs-site.xml
 
   echo "Datanode"
@@ -46,13 +46,9 @@ if  [[ $HDFS == "YES" ]]; then
   hdfs datanode &
 fi
 
-
-echo "Worker: $HOSTNAME Connected to spark://$SPARK_MASTER_HOSTNAME:$SPARK_MASTER_PORT "
+echo "Worker: $HOSTNAME Connected to spark://$SPARK_MASTER_HOSTNAME:$SPARK_MASTER_PORT --webui-port $SPARK_WORKER_WEBUI_PORT"
 
 source ~/.bashrc
 
 # Starting the Spark Worker
-
-echo "$SPARK_HOME/sbin/start-master.sh -h $HOSTNAME -p $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT"
-
-$SPARK_HOME/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker spark://$SPARK_MASTER_HOSTNAME:$SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT
+$SPARK_HOME/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker spark://$SPARK_MASTER_HOSTNAME:$SPARK_MASTER_PORT --webui-port $SPARK_WORKER_WEBUI_PORT
